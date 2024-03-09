a57ed01c5f956ff214e798b7667649c0628d3e10
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-09 12:47:45 +0530

    userGlobalStore created with zustand and persisted using async storage

--- client/task-management-app/src/navigation/index.tsx
+++ client/task-management-app/src/navigation/index.tsx
@@ -1,13 +1,14 @@
+import useUserGlobalStore from "@/store/useUserGlobalStore";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import AuthStackNavigator from "./auth-stack-navigator";
 import AppStackNavigator from "./app-stack-navigator";
 
 const Navigation = () => {
+  const { user } = useUserGlobalStore();
   return (
     <NavigationContainer>
-      {/* <AuthStackNavigator /> */}
-      <AppStackNavigator />
+      {user ? <AppStackNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 };
---
+++ client/task-management-app/src/types/index.d.ts
@@ -0,0 +1,11 @@
+interface IUser {
+    email: string
+    name: string
+    password: string
+  }
+  
+ export interface IAuthenticatedUser {
+    email: string
+    name: string
+  }
+  
